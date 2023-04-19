1c1ed1f519b883c673e2a99230612b5078788f94
priest-ops <duncan.otuoma@gmail.com>
2023-04-19 17:12:08 +0300

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-#from src.logger import logging
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
@@ -16,4 +15,6 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
+
+
+
